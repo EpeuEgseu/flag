6331695f6e127dbca5f6f2e783ba709f2c848bf9
hax0r <a@hax0r.info>
2015-12-25 10:23:09 +0000

    re 2015.12.25

--- _flag.js
+++ _flag.js
@@ -2,35 +2,43 @@
 * flag v0.1
 * Copyright 2015.12.24
 * flag is Javascript library for non-blocking notifications. this plugin don't need to jquery library
-*if you have someting, please contact this email here('hacking4soju at gmail.com' or jhjang1005@naver.com)
+*if you have someting, please contact this email here('hacking4soju at gmail.com' or 'jhjang1005@naver.com')
 */
 
-(function(define){
+(function(){
+  'use strict';
     try{
-        var flag = flag || {} || new Object();
-        var self = this;
-        flag.settings.lib = 'inc';
-        flag.Type = {
-            info : 'info',
-            success : 'success',
-            warning : 'warning',
-            debug : function(){
-                'use strict';
-                require(flag.settings.lib+'_debug.js'); //디버깅할때만..strictly하게
-            }
-        }
-        Object.defineProperty(exports, '__esModule', {
-          value: true
+      if(!window.flag){
+        window.flag = flag;
+      }
+      var flag = flag || {} || new Object();
+      var self = this;
+      var low_client = false;
+      var flag = {
+          info : info,
+          success : 'success',
+          warning : 'warning',
+          error : 'error',
+          debug : function(){
+            console.log(this);
+              // require(flag.settings.lib+'_debug.js'); //디버깅할때만..strictly하게
+          }
+      }
+      function info(){
+        console.log('is info');
+      }
+      if(window.navigator.appName == 'Microsoft Internet Explorer' && window.attachEvent && !window.addEventListener){//익스8까지만 서포트
+        window.navigator.__defineGetter__('userAgent', function(){
+          low_client = true;
         });
-        if(window.navigator.appName == 'Microsoft Internet Explorer' && window.attachEvent && !window.addEventListener){//익스8까지만 서포트
-            window.navigator.__defineGetter__('userAgent', function(){
-                return 'fake';
-            });
-        }
-        exports['default'] = flag = function(){
-
-        }
-        flag.statusCode = 200;
+      }
+      // Object.defineProperty(exports, '__esModule', {
+      //   value: true
+      // });
+      // exports['default'] = flag = function(){
+      //
+      // }
+      console.log('test');
     }catch(e){
         switch (true){
             case (e instanceof EvalError):
@@ -44,4 +52,4 @@
                 break;
         }
     }
-});
+})();
