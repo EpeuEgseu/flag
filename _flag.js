b5c2239f86e42f03e60d92bb8b25ba66cbcd4e3c
humit <jhjang1005@naver.com>
2015-12-26 14:44:57 +0900

    media query add.

--- _flag.js
+++ _flag.js
@@ -4,7 +4,6 @@
 * flag is Javascript library for non-blocking notifications. this plugin don't need to jquery library
 *if you have someting, please contact this email here('hacking4soju at gmail.com' or 'jhjang1005@naver.com')
 */
-
 (function(){
   'use strict';
     try{
@@ -21,6 +20,9 @@
           },
           low_client : false,
           no:0
+      }
+      if(window.navigator.appName == 'Microsoft Internet Explorer' && window.attachEvent && !window.addEventListener){//익스8까지만 서포트
+        flag.low_client = true;
       }
       function initialize(position){
         if(!flag.parentNode){
@@ -42,36 +44,81 @@
 
       function info(content, title){
         console.log('is info');
-        createNode({type:'info', content:content, title:title});
+        createNode({type:'info', content:content, title:title, size:'is-middle'});
       }
-      function success(){
+      function success(content, title){
         console.log('is success');
+        createNode({type:'success', content:content, title:title, size:'is-middle'});
       }
-      function warning(){
+      function warning(content, title){
         console.log('is warning');
+        createNode({type:'warning', content:content, title:title, size:'is-middle'});
       }
-      function error(){
+      function error(content, title){
         console.log('is error');
+        createNode({type:'error', content:content, title:title, size:'is-middle'});
       }
-      if(window.navigator.appName == 'Microsoft Internet Explorer' && window.attachEvent && !window.addEventListener){//익스8까지만 서포트
-        window.navigator.__defineGetter__('userAgent', function(){
-          flag.low_client = true;
-        });
+
+      var removeNode = function(_node,effect){
+        console.debug(_node);
+        // var _node = _node.parentNode;
+        if(!effect){
+          effect = 'fade-out';
+        }
+        if(_node){
+          switch (effect) {
+            case 'fade-out':
+              var op = 1;
+              var timer = setInterval(function () {
+                  if (op <= 0.1){
+                      console.log(timer+'싀싀시ㅡ븨');
+                      clearInterval(timer);
+                      // _node.style.display = 'none';
+                      _node.parentNode.removeChild(_node);
+                  }
+                  _node.style.opacity = op;
+                  if(flag.low_client){
+                    _node.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                  }
+                  op -= op * 0.1;
+              }, 50);
+              break;
+          }
+        }
       }
       // type, content, title
       function createNode(option){
+        if(!option.content){
+          option.content = 'display default message';
+        }
+        if(!option.title){
+          // console.log('yes i"m here');
+          switch (option.type) {
+            case 'info': option.title = 'Info';
+              break;
+            case 'success': option.title = 'Success';
+              break;
+            case 'warning': option.title = 'Warning';
+              break;
+            case 'error' : option.title = 'Error'
+              break;
+          }
+        }
+        if(!option.size){
+          option.size = 'is-middle';
+        }
         var div = document.createElement('div');
         var attr = document.createAttribute('class');
-        attr.value = '_flag_dom '+option.type;
+        attr.value = '_flag_dom '+option.type+' '+option.size;
         div.setAttributeNode(attr);
-        attr = document.createAttribute('id');
+        attr = document.createAttribute('data-flag-index');
         attr.value = ++flag.no;
         div.setAttributeNode(attr);
 
         var title_tag = document.createElement('h3');
         title_tag.appendChild(document.createTextNode(option.title));
         div.appendChild(title_tag);
-        
+
         var content_tag = document.createElement('p')
         content_tag.appendChild(document.createTextNode(option.content));
         div.appendChild(content_tag);
@@ -79,31 +126,33 @@
             console.log("You need to initialize!");
         }
         flag.parentNode.appendChild(div);
+        setTimeout(function(){
+          removeNode(div);
+        },5000);// 5000 is default sec
       }
 
       if(!window.flag){
           window.flag = flag;
       }
-      // Object.defineProperty(exports, '__esModule', {
-      //   value: true
-      // });
-      // exports['default'] = flag = function(){
-      //
-      // }
-      if(!window.flag){
-        window.flag = flag;
-      }
     }catch(e){
         switch (true){
             case (e instanceof EvalError):
-                alert("Eval 에러: " + e.message);
+              console.log("Eval 에러: " + e.message);
                 break;
             case (e instanceof RangeError):
-                alert("RangeError에러: " + e.message);
+              console.log("RangeError 에러: " + e.message);
                 break;
             default:
-                alert("에러: " + e.message);
+              console.log("에러: " + e.message);
                 break;
         }
     }
 })();
+
+flag.info("Content", "Title");
+flag.success();
+
+flag.success("Content", "Title");
+flag.info("Content", "Title");
+flag.success("Content", "Title");
+flag.success("Content", "Title");
